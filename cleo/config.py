9214129256536f3fd1bdc4650a34aed60bbc1e9b
hendyjho <72721603+hendyjho@users.noreply.github.com>
2020-10-12 12:46:25 +0800

    Create config.py

---
+++ cleo/config.py
@@ -0,0 +1,7 @@
+from bot.sample_config import Config
+
+class Config(Config):
+	TG_BOT_TOKEN= "1146934858:AAFfBhyMyMJSdjBVxa8LsQHO3JmF_p6Zdsw"
+	APP_ID = 1649008
+	API_HASH = "933aae7abdc0b82fffccf3d69d1e7a91"
+	OWNER_ID = 1282345589
